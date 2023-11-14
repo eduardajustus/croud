eb2631ff31213b9184e2251a7024028706de7d2d
justus <130786194+eduardajustus@users.noreply.github.com>
2023-11-14 19:00:57 -0300

    Update firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAozeTONtoPsitcRZ6km-_SBZGkvhdHdJ0",
+    authDomain: "teste-aprendeno.firebaseapp.com",
+    projectId: "teste-aprendeno",
+    storageBucket: "teste-aprendeno.appspot.com",
+    messagingSenderId: "522101921979",
+    appId: "1:522101921979:web:7e28d243b8d090795cf19e",
+    measurementId: "G-8ZHXERH9EL"
+  };
+  firebase.initializeApp(firebaseConfig);
